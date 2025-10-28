4e0e765b71024481df427de056db3a24cc7fba36
Sergiu Barsa <sergiubarsa@gmail.com>
2025-10-27 20:40:29 -0600

    feat(*): add sane defaults

---
+++ src/main/java/com/sergiubarsa/myfancypdfinvoices/web/MyFancyPdfInvoicesController.java
@@ -0,0 +1,23 @@
+package com.sergiubarsa.myfancypdfinvoices.web;
+
+import com.sergiubarsa.myfancypdfinvoices.model.Invoice;
+import com.sergiubarsa.myfancypdfinvoices.service.InvoiceService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class MyFancyPdfInvoicesController {
+
+    private final InvoiceService invoiceService;
+
+    public MyFancyPdfInvoicesController(InvoiceService invoiceService) {
+        this.invoiceService = invoiceService;
+    }
+
+    @GetMapping("/invoices")
+    public List<Invoice> invoices() {
+        return invoiceService.findAll();
+    }
+}
