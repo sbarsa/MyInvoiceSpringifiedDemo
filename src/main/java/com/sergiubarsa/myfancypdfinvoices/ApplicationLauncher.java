e999af7146a92ad4828d87de0e2778a957deb1ca
Sergiu Barsa <sergiubarsa@gmail.com>
2025-10-27 20:28:54 -0600

    feat(*): apply dispatcher servlet

--- src/main/java/com/sergiubarsa/myfancypdfinvoices/ApplicationLauncher.java
+++ src/main/java/com/sergiubarsa/myfancypdfinvoices/ApplicationLauncher.java
@@ -1,10 +1,14 @@
 package com.sergiubarsa.myfancypdfinvoices;
 
-import com.sergiubarsa.myfancypdfinvoices.web.MyFancyPdfInvoiceServlet;
+import com.sergiubarsa.myfancypdfinvoices.context.ApplicationConfiguration;
+import jakarta.servlet.ServletContext;
 import org.apache.catalina.Context;
 import org.apache.catalina.LifecycleException;
 import org.apache.catalina.Wrapper;
 import org.apache.catalina.startup.Tomcat;
+import org.springframework.web.context.WebApplicationContext;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+import org.springframework.web.servlet.DispatcherServlet;
 
 public class ApplicationLauncher {
 
@@ -15,10 +19,20 @@ public class ApplicationLauncher {
         tomcat.getConnector();
 
         Context ctx = tomcat.addContext("", null);
-        Wrapper servlet = Tomcat.addServlet(ctx, "myFirstServlet", new MyFancyPdfInvoiceServlet());
+        WebApplicationContext appCtx = createApplicationContext(ctx.getServletContext());
+        Wrapper servlet = Tomcat.addServlet(ctx, "dispatcherServlet", new DispatcherServlet(appCtx));
         servlet.setLoadOnStartup(1);
         servlet.addMapping("/*");
 
         tomcat.start();
+    }
+
+    private WebApplicationContext createApplicationContext(ServletContext servletContext) {
+        AnnotationConfigWebApplicationContext ctx = new AnnotationConfigWebApplicationContext();
+        ctx.register(ApplicationConfiguration.class);
+        ctx.setServletContext(servletContext);
+        ctx.refresh();
+        ctx.registerShutdownHook();
+        return ctx;
     }
 }
