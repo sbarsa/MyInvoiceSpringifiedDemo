91d1238477491bdd69e3dd179f24b67011a1f447
Sergiu Barsa <sergiubarsa@gmail.com>
2025-10-29 18:27:27 -0600

    feat(*): add advice exception handling

---
+++ src/main/java/com/sergiubarsa/myfancypdfinvoices/web/GlobalControllerExceptionHandler.java
@@ -0,0 +1,26 @@
+package com.sergiubarsa.myfancypdfinvoices.web;
+
+
+import jakarta.validation.ConstraintViolationException;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalControllerExceptionHandler {
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public String handleMethodArgumentNotValid(MethodArgumentNotValidException exception) {
+        return "Sorry, that was not quite right: " + exception.getMessage();
+    }
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(ConstraintViolationException.class)
+    public String handleConstraintViolation(ConstraintViolationException exception) {
+        return "Sorry, that was not quite right: " + exception.getMessage();
+    }
+
+}
