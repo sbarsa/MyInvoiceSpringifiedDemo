116ce83394f8b65b8f0b5e237bafa8d7101719d4
Sergiu Barsa <sergiubarsa@gmail.com>
2025-11-01 11:59:02 -0600

    feat(*): add login page

--- src/main/java/com/sergiubarsa/myfancypdfinvoices/web/InvoicesHtmlController.java
+++ src/main/java/com/sergiubarsa/myfancypdfinvoices/web/InvoicesHtmlController.java
@@ -1,10 +1,13 @@
 package com.sergiubarsa.myfancypdfinvoices.web;
 
 import com.sergiubarsa.myfancypdfinvoices.service.InvoiceService;
+import com.sergiubarsa.myfancypdfinvoices.web.forms.LoginForm;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.time.LocalDateTime;
@@ -24,5 +27,21 @@ public class InvoicesHtmlController {
         model.addAttribute("username", username);
         model.addAttribute("currentDate", LocalDateTime.now());
         return "index.html";
+    }
+
+    @GetMapping("/login")
+    public String login(Model model) {
+        model.addAttribute("loginForm", new LoginForm());
+        return "login.html";
+    }
+
+    @PostMapping("/login")
+    public String login(@ModelAttribute LoginForm loginForm, Model model){
+        //for POC purposes, we're assuming that any combinations of username == password are correct
+        if (loginForm.getUsername().equals(loginForm.getPassword())) {
+            return "redirect:/";
+        }
+        model.addAttribute("invalidCredentials", "true");
+        return "login.html";
     }
 }
---
+++ src/main/java/com/sergiubarsa/myfancypdfinvoices/web/forms/LoginForm.java
@@ -0,0 +1,23 @@
+package com.sergiubarsa.myfancypdfinvoices.web.forms;
+
+public class LoginForm {
+
+    private String username;
+    private String password;
+
+    public LoginForm() {
+    }
+
+    public LoginForm(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+}
