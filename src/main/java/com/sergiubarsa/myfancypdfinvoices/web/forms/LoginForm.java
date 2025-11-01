e3bf3027130736474d06d4ea384eaa3c9347e13e
Sergiu Barsa <sergiubarsa@gmail.com>
2025-11-01 12:01:52 -0600

    fix(*): add setters to satisfy Spring

--- src/main/java/com/sergiubarsa/myfancypdfinvoices/web/forms/LoginForm.java
+++ src/main/java/com/sergiubarsa/myfancypdfinvoices/web/forms/LoginForm.java
@@ -20,4 +20,12 @@ public class LoginForm {
     public String getUsername() {
         return username;
     }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
 }
